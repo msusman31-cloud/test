282ca365a252e156cca00ee331f1814fd690fbb7
msusman31-cloud <msusman31@gmail.com>
2026-05-14 02:56:34 -0700

    add echo to testq.sh

--- testq.sh
+++ testq.sh
@@ -1,4 +1,8 @@
-echo "hellow"
-echo "hi"
-echo "echi"
-echo "echi"
+#!/bin/bash
+
+echo "Current time in US Time Zones:"
+echo "EST: $(TZ="America/New_York" date)"
+echo "CST: $(TZ="America/Chicago" date)"
+echo "MST: $(TZ="America/Denver" date)"
+echo "PST: $(TZ="America/Los_Angeles" date)"
+echo "Update complete"
